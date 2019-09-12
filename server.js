3eb63ccb7c09cf1c059ea96893d86c00593da3c2
unknown <newmohib@gmail.com>
2019-09-12 17:40:35 +0600

    passport google success

--- server.js
+++ server.js
@@ -2,50 +2,94 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-const bcrypt=require('bcrypt');
-const passport=require('passport');
-const flash=require('express-flash');
-const session=require('express-session');
-const methodOverride=require('method-override');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;;
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const GoogleStrategy = require('passport-google-oauth20');
+//const OktaStrategy = require('passport-okta-oauth')
+
+//
+
+
+app.set('view-engine', 'ejs');
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(session({
+//     secret:process.env.SESSION_SECRET,
+//     resave:false,
+//     saveUninitialized:false,
+// }));
+
+
+
+const authenticateUser = (accessToken, refreshToken, userinfo, cb,done) => {
+   
+    //console.log("userinfo",userinfo);
+    //console.log("cd",cb);
+    if (userinfo == null) {
+        return done(null, false, { message: 'No user with that email' })
+    }else{
+        return done(null, userinfo)
+    }
+
+}
 
 passport.use(new GoogleStrategy({
     clientID: "692806867880-73s3obdnmg1uc5et4bv3jt50dbfuqscr.apps.googleusercontent.com",
     clientSecret: "ZLrdxMmDXzSW6d2gmUpCRp74",
     callbackURL: "http://localhost:4000/oauth"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+}, authenticateUser))
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
 
-// passport.serializeUser(function(user, done) {
-//     done(null, user);
-//   });
-  
-//   passport.deserializeUser(function(obj, done) {
-//     done(null, obj);
-//   });
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
 
-  app.get('/', function(req, res){
-    res.render('Login success');
-  });
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/oauth', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+//medle
 
 
 
 
 
 
+app.get('/',(req,res)=>{
+    res.send('Login success');
+});
+app.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/oauth',
+    passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+
+    }),
+
+);
+
+
+function checkAuthenticated(req,res,next){
+    console.log('checkAuthenticated');
+    if (req.isAuthenticated()) {
+        console.log('authenticated');
+        return next();
+    }
+    res.redirect('/google');
+}
+
+function checkNotAuthenticated(req,res,next){
+ if (req.isAuthenticated()) {
+   return res.redirect('/google')
+ }
+ return next() 
+}
+
+//midle
 
 
 
@@ -54,6 +98,12 @@ app.get('/oauth',
 
 
 
-app.listen(port,function(e){
-    console.log('Server running at port',port);
+
+
+
+
+
+
+app.listen(port, function (e) {
+    console.log('Server running at port', port);
 });
